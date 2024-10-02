40f18bda312826ff4b85a5ca7d968013833e6c1e
shatha <sha7cs@gmail.com>
2024-10-03 02:13:12 +0300

    comment in models

--- GP_app/models.py
+++ GP_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# categories (id, category)
+# items (id, name, desc, category_id)
+# transactions (id , item, quantity, price, tax, discount, total) we may a column that has costumer id but would be optional only applies for loyalty app users
+# costumers (id, firstname, lastname, phone, email) -> only if they have some loyalty system بس كيف نربطه مع الترانزاكشن؟
