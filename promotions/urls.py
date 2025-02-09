2537c64f570548eba0952135ffd99411190e63da
shatha <sha7cs@gmail.com>
2025-02-09 17:38:25 +0300

    made the language btn dynamic

--- promotions/urls.py
+++ promotions/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('' , views.promotions_view , name='promotions'),
-    path('set-lang/', views.set_language, name='set_language'),
+    path('set-lang/<str:urlname>/', views.set_language, name='set_language'),
     path('' , views.chatbot , name='promotions'),
 ]
