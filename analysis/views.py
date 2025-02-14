a806e7ae6a87b42f06f5dd87a5e7f2fad26dd075
JumanahAlmatrood <macbookpro@MacBooks-MacBook-Pro.local>
2025-02-14 19:45:49 +0300

    get the data from firebase

--- analysis/views.py
+++ analysis/views.py
@@ -7,37 +7,55 @@ from django.utils.translation import gettext_lazy as _
 from django. utils.translation import get_language, activate
 from django.urls import reverse
 from django.shortcuts import redirect
-from authentication_app.decorators import allowed_users, admin_only, unauthenticated_user
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from firebase_admin import firestore
 
-file_path = "Data/Sales_ARS.csv"
-df = pd.read_csv(file_path)
-df['business_date'] = pd.to_datetime(df['business_date'], dayfirst=True, errors='coerce')
-df = df.dropna(subset=['business_date', 'total_price'])
 
+db = firestore.client()
+
+def get_sales_data():
+    #sales_ref = db.collection('Sales_ARS').limit(1000)
+    sales_ref = db.collection('Sales_ARS') 
+    docs = sales_ref.stream()
+
+    data = []
+    for doc in docs:
+        record = doc.to_dict()
+        try:
+            record["detailed_orders"] = json.loads(record["detailed_orders"].replace("'", '"'))
+        except json.JSONDecodeError:
+            record["detailed_orders"] = []
+
+        data.append(record)
+
+    df = pd.DataFrame(data)
+    
+    df['business_date'] = pd.to_datetime(df['business_date'], dayfirst=True, errors='coerce')
+    df = df.dropna(subset=['business_date', 'total_price'])
+
+    return df
 
 def set_language(request):
-    language = request.GET['language']
+    language = request.GET.get('language')
     if language:
         activate(language)
         request.session['django_language'] = language 
     next_url = request.META.get('HTTP_REFERER', '/')
     return redirect(next_url) 
-@login_required
-@allowed_users(allowed_roles=['normal_user'])
+
 def analysis_view(request):
+
     try:
+        df = get_sales_data()
+
         total_sales = df['total_price'].sum()
         total_transactions = len(df)
 
-        detailed_orders = df['detailed_orders'].dropna().apply(ast.literal_eval)
+        detailed_orders = df['detailed_orders'].dropna()
         all_items = [item['item'] for order in detailed_orders for item in order]
         best_seller = pd.Series(all_items).value_counts().idxmax()
 
         monthly_sales = df.groupby(df['business_date'].dt.month)['total_price'].sum().to_dict()
 
-     
         if len(monthly_sales) >= 2:
             months = sorted(monthly_sales.keys())
             current_month = months[-1]
@@ -53,8 +71,6 @@ def analysis_view(request):
             for item in order:
                 category = item['category']
                 category_sales[category] = category_sales.get(category, 0) + item['quantity']
-        
-        
 
         context = {
             'total_sales': total_sales,
@@ -65,24 +81,28 @@ def analysis_view(request):
             'category_labels': list(category_sales.keys()),
             'category_data': list(category_sales.values()),
         }
-        
-                    
+
         return render(request, 'analysis/analysis.html', context)
     except Exception as e:
         return render(request, 'analysis/error.html', {'error': str(e)})
 
-
 def filter_data(request):
-    filter_type = request.GET.get('filter', 'month')
-    selected_month = request.GET.get('month', None)
+    try:
+        df = get_sales_data()
 
-    if filter_type == 'day' and selected_month:
-        month_number = int(selected_month)
-        filtered_df = df[df['business_date'].dt.month == month_number]
-        sales_data = filtered_df.groupby(filtered_df['business_date'].dt.day)['total_price'].sum().to_dict()
-    elif filter_type == 'month':
-        sales_data = df.groupby(df['business_date'].dt.month)['total_price'].sum().to_dict()
-    else:
-        sales_data = {}
+        filter_type = request.GET.get('filter', 'month')
+        selected_month = request.GET.get('month', None)
 
-    return JsonResponse({'sales': sales_data})
+        if filter_type == 'day' and selected_month:
+            month_number = int(selected_month)
+            filtered_df = df[df['business_date'].dt.month == month_number]
+            sales_data = filtered_df.groupby(filtered_df['business_date'].dt.day)['total_price'].sum().to_dict()
+        elif filter_type == 'month':
+            sales_data = df.groupby(df['business_date'].dt.month)['total_price'].sum().to_dict()
+        else:
+            sales_data = {}
+
+        return JsonResponse({'sales': sales_data})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
